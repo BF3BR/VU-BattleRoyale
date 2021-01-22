c23540035077fb7c58dac69c131ed06833e73d87
kaloczikvn <kaloczi.kvn@gmail.com>
2021-01-22 23:25:46 +0100

    uicleanup cleanup

--- ext/Client/UICleanup.lua
+++ ext/Client/UICleanup.lua
@@ -1,44 +1,47 @@
 class 'UICleanup'
 
 function UICleanup:__init()
-    Hooks:Install('UI:PushScreen', 999, function(p_Hook, p_Screen, p_GraphPriority, p_ParentGraph)
-        local s_Screen = UIGraphAsset(p_Screen)
+    self.m_PartitionLoadedEvent = Events:Subscribe("Partition:Loaded", self, self.OnPartitionLoaded)
+    self.m_UIPushScreenHook =  Hooks:Install('UI:PushScreen', 999, self, self.OnUIPushScreen)
+end
 
-        -- print("INFO: Ui screen pushed: " .. s_Screen.name)
-        
-        if s_Screen.name == 'UI/Flow/Screen/SpawnScreenPC' or
-            s_Screen.name == 'UI/Flow/Screen/SpawnScreenTicketCounterConquestScreen' or
-            --s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsScreen' or
-            s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD32Screen' or
-            s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD16Screen' or
-            s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD64Screen' or
-            s_Screen.name == 'UI/Flow/Screen/KillScreen' or
-            s_Screen.name == 'UI/Flow/Screen/SpawnButtonScreen' then
-                p_Hook:Return(nil)
-            return
-        end
-        
-	    if 	s_Screen.name == 'UI/Flow/Screen/HudConquestScreen' then
-            local s_Clone = s_Screen:Clone(s_Screen.instanceGuid)
-            local s_ScreenClone = UIGraphAsset(s_Clone)
-
-            for i = #s_Screen.nodes, 1, -1 do
-                local node = s_Screen.nodes[i]
-                if node ~= nil then
-                    if node.name == 'TicketCounter' or 
-                        node.name == 'HudBackgroundWidget' or
-                        node.name == 'CapturepointManager' or
-                        node.name == 'ObjectiveBar'
-                    then
-                        s_ScreenClone.nodes:erase(i)
+function UICleanup:OnPartitionLoaded(p_Partition)
+    if p_Partition.name == 'ui/flow/screen/hudconquestscreen' then
+        for _, instance in pairs(p_Partition.instances) do
+            if instance:Is('UIScreenAsset') then
+                instance = UIScreenAsset(instance)
+                instance:MakeWritable()
+                for i = #instance.nodes, 1, -1 do
+                    local node = instance.nodes[i]
+                    if node ~= nil then
+                        if node.name == 'TicketCounter' or 
+                            node.name == 'HudBackgroundWidget' or
+                            node.name == 'CapturepointManager' or
+                            node.name == 'ObjectiveBar'
+                        then
+                            instance.nodes:erase(i)
+                        end
                     end
                 end
             end
-
-            p_Hook:Pass(s_ScreenClone, p_GraphPriority, p_ParentGraph)
-            return
         end
-    end)
+    end
+end
+
+function UICleanup:OnUIPushScreen(p_Hook, p_Screen, p_GraphPriority, p_ParentGraph)
+    local s_Screen = UIGraphAsset(p_Screen)
+            
+    if s_Screen.name == 'UI/Flow/Screen/SpawnScreenPC' or
+        s_Screen.name == 'UI/Flow/Screen/SpawnScreenTicketCounterConquestScreen' or
+        --s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsScreen' or
+        s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD32Screen' or
+        s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD16Screen' or
+        s_Screen.name == 'UI/Flow/Screen/Scoreboards/ScoreboardTwoTeamsHUD64Screen' or
+        s_Screen.name == 'UI/Flow/Screen/KillScreen' or
+        s_Screen.name == 'UI/Flow/Screen/SpawnButtonScreen' then
+            p_Hook:Return(nil)
+        return
+    end
 end
 
 return UICleanup()
