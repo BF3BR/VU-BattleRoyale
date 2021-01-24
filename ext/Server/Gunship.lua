763b283c21c139435a36ce687e29309e6c64cba2
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-01-24 18:24:31 +0100

    update gunship

--- ext/Server/Gunship.lua
+++ ext/Server/Gunship.lua
@@ -5,12 +5,17 @@ function Gunship:__init(p_Match)
 	self.m_Match = p_Match
 	
 	self.m_JumpOutOfGunshipEvent = NetEvents:Subscribe('JumpOutOfGunship', self, self.OnJumpOutOfGunship)
+	self.m_EngineUpdateEvent = Events:Subscribe('Engine:Update', self, self.OnEngineUpdate)
+	
+	
+	self.m_SetFlyPath = false
+	self.m_CumulatedTime = 0
+	
 end
 
 function Gunship:OnJumpOutOfGunship(p_Player)
 	
 	-- Get the Gunship transform
-	-- TODO: Default transform should be the endposition of the gunship
 	local s_Transform = nil
 	
 	local s_VehicleSpawnEntityIterator = EntityManager:GetIterator("ServerVehicleEntity")
@@ -34,41 +39,79 @@ function Gunship:OnJumpOutOfGunship(p_Player)
 	
 	self.m_Match:SpawnPlayer(p_Player, s_Transform)
 end
+
+function Gunship:OnEngineUpdate(p_DeltaTime)
+	if not self.m_SetFlyPath then
+		return
+	end
+	self.m_CumulatedTime = self.m_CumulatedTime + p_DeltaTime
+	if self.m_CumulatedTime >= 0.1 then
+		self.m_SetFlyPath = false
+		self.m_CumulatedTime = 0
+		self:SetLocatorEntityTransform(true)
+		self:SetVehicleEntityTransform()
+		self:SetLocatorEntityTransform(false)
+		NetEvents:BroadcastLocal('GunshipCamera')
+	end
+end
 	
 function Gunship:Spawn()
-	print("INFO: Spawning the gunship.")
-	NetEvents:BroadcastLocal('GunshipCamera')
 	
 	local s_VehicleSpawnEntityIterator = EntityManager:GetIterator("ServerVehicleSpawnEntity")
 	local s_VehicleSpawnEntity = s_VehicleSpawnEntityIterator:Next()
 	
 	while s_VehicleSpawnEntity do
+		
 		if s_VehicleSpawnEntity.data.instanceGuid == Guid('5449C054-7A18-4696-8AA9-416A8B9A9CD0') then
+			
 			s_VehicleSpawnEntity = Entity(s_VehicleSpawnEntity)
 			s_VehicleSpawnEntity:FireEvent("Spawn")
-			break
-		end
-		s_VehicleSpawnEntity = s_VehicleSpawnEntityIterator:Next()
-	end
-	
-	local s_SequenceEntityIterator = EntityManager:GetIterator("SequenceEntity")
-	local s_SequenceEntity = s_SequenceEntityIterator:Next()
-	
-	while s_SequenceEntity do
-		if s_SequenceEntity.data.instanceGuid == Guid('20F64BEB-665E-4C34-8E01-744EACD1BD16') then
-			s_SequenceEntity = Entity(s_SequenceEntity)
-			s_SequenceEntity:FireEvent("Start")
-			s_SequenceEntity:RegisterEventCallback(self, self.OnSequenceEntityEventCallback)
+			
+			self.m_SetFlyPath = true
 			return
+			
 		end
-		s_SequenceEntity = s_SequenceEntityIterator:Next()
+		
+		s_VehicleSpawnEntity = s_VehicleSpawnEntityIterator:Next()
+	
 	end
 end
 
-function Gunship:OnSequenceEntityEventCallback(ent, entityEvent)
-	if entityEvent.eventId == 229946160 then -- "Reset"
-		print("INFO: OnSequenceEntityEventCallback - Reset")
-		NetEvents:BroadcastLocal('ForceJumpOufOfGunship')
+function Gunship:SetVehicleEntityTransform()
+	local s_VehicleEntityIterator = EntityManager:GetIterator("ServerVehicleEntity")
+	local s_VehicleEntity = s_VehicleEntityIterator:Next()
+	
+	while s_VehicleEntity do
+			
+		if s_VehicleEntity.data.instanceGuid == Guid('81ED68CF-5FDE-4C24-A6B4-C38FB8D4A778') then
+			
+			s_VehicleEntity = SpatialEntity(s_VehicleEntity)
+			
+			local s_StartTransform = LinearTransform(Vec3(-0.459440, 0.000000, -0.888209), Vec3(0.000000, 1.000000, 0.000000), Vec3(0.888209, 0.000000, -0.459440), Vec3(-528.583984, 556.093933, -327.112305))
+			s_VehicleEntity.transform = s_StartTransform
+
+			break
+		end
+		s_VehicleEntity = s_VehicleEntityIterator:Next()
+	
+	end
+end
+
+function Gunship:SetLocatorEntityTransform(p_First)
+	local s_LocatorEntityIterator = EntityManager:GetIterator("LocatorEntity")
+	local s_LocatorEntity = s_LocatorEntityIterator:Next()
+	
+	while s_LocatorEntity do
+		
+		s_LocatorEntity = SpatialEntity(s_LocatorEntity)
+		local s_DirectionTransform = LinearTransform(Vec3(-0.459440, 0.000000, -0.888209), Vec3(0.000000, 1.000000, 0.000000), Vec3(0.888209, 0.000000, -0.459440), Vec3(-491.778320, 553.822815, -346.150391))
+		if p_First then
+			s_DirectionTransform.trans.y = 556.093933
+		end
+		s_LocatorEntity.transform = s_DirectionTransform
+		
+		s_LocatorEntity = s_LocatorEntityIterator:Next()
+	
 	end
 end
 
