f2e2ce416015880e60c7d0e4f44ea01cc64520e7
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-02-01 18:34:45 +0100

    add Whitelist

---
+++ ext/Server/Whitelist.lua
@@ -0,0 +1,26 @@
+class 'Whitelist'
+
+function Whitelist:__init()
+    self.whitelistedNames = {
+        "voteban_flash",
+        "Bree",
+        "Janssent",
+        "KVN",
+        "breaknix",
+        "kiwidog",
+        "kiwidoggie",
+        "keku645"
+    }
+    Hooks:Install('Player:RequestJoin', 100, self, self.OnPlayerRequestJoin) 
+end
+
+function Whitelist:OnPlayerRequestJoin(hook, joinMode, accountGuid, playerGuid, playerName)
+    for _, name in pairs(self.whitelistedNames) do
+        if playerName:lower() == name:lower() then
+            return
+        end
+    end
+    hook:Return(false)
+end
+
+g_Whitelist = Whitelist()
