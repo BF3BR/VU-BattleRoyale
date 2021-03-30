e7fb2d87a49b152f0615d51e781488ae9f82e74f
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-03-30 23:29:45 +0200

    fix UISquadCompData error when using maploader
    ```
    [22:30:11] [error] [VeniceEXT] Error: [string "UIManager.lua"]:54: sol: no matching function call takes this number of arguments and the specified types
    stack traceback:
    [C]: in global 'UISquadCompData'
    [string "UIManager.lua"]:54: in function <[string "UIManager.lua"]:34>
    ```

--- ext/Client/UIManager.lua
+++ ext/Client/UIManager.lua
@@ -3,17 +3,22 @@ class "UIManager"
 local m_ConnectionHelper = require("__shared/Utils/ConnectionHelper")
 
 local m_MPMenuScreenAsset = FrostbiteDC{
-    partitionGuid = Guid('993445AF-2476-11E0-834E-C984E80F7234'), 
-    instanceGuid = Guid('5FE2571D-D0AD-CF75-3CB6-43A43AFC0E8B')
+	partitionGuid = Guid('993445AF-2476-11E0-834E-C984E80F7234'), 
+	instanceGuid = Guid('5FE2571D-D0AD-CF75-3CB6-43A43AFC0E8B')
+}
+
+local m_UISquadCompData = FrostbiteDC{
+	partitionGuid = Guid('88DECC5B-43E8-11E0-A213-8C5E94EEBB5D'), 
+	instanceGuid = Guid('538F9596-5BED-84BC-92E6-99595A9A69E5 ')
 }
 
 local m_WidgetIndexes = {
 	itemList = 1,
 	itemListHeader = 2,
-    gamemodeInfo = 5,
+	gamemodeInfo = 5,
 	gamemodeInfoHeader = 4,
 	map = 10,
-    mapHeader = 3,
+	mapHeader = 3,
 }
 
 local m_MenuItems = {
@@ -24,15 +29,15 @@ local m_MenuItems = {
 }
 
 function UIManager:__init()
-    self:RegisterEvents()
+	self:RegisterEvents()
 end
 
 function UIManager:RegisterEvents()
-    m_MPMenuScreenAsset:RegisterLoadHandler(self, self.ModifyMenu)
+	m_MPMenuScreenAsset:RegisterLoadHandler(self, self.ModifyMenu)
 end
 
 function UIManager:ModifyMenu(p_Instance)
-    local s_ScreenAsset = UIScreenAsset(p_Instance)
+	local s_ScreenAsset = UIScreenAsset(p_Instance)
 	s_ScreenAsset:MakeWritable()
 
 	-- Replace menu items
@@ -51,7 +56,12 @@ function UIManager:ModifyMenu(p_Instance)
 	s_CenterHeaderBinding:MakeWritable()
 	s_CenterHeaderBinding.header = UIDataSourceInfo()		-- remove dynamic gamemode lookup
 	s_CenterHeaderBinding.staticHeader = "YOUR SQUAD"
-	s_CenterHeaderBinding.subHeader.dataCategory = UISquadCompData(ResourceManager:SearchForDataContainer('UI/UIComponents/UISquadComp'))
+	local s_UISquadComp = ResourceManager:SearchForDataContainer('UI/UIComponents/UISquadComp')
+	if s_UISquadComp ~= nil then
+		s_CenterHeaderBinding.subHeader.dataCategory = UISquadCompData(s_UISquadComp)
+	else
+		m_UISquadCompData:RegisterLoadHandler(self, self.FinishModifyMenu)
+	end
 	s_CenterHeaderBinding.subHeader.dataKey = -917467149
 	
 	-- Set map header text
@@ -65,15 +75,27 @@ function UIManager:ModifyMenu(p_Instance)
 	s_ScreenAsset.nodes:erase(m_WidgetIndexes.gamemodeInfo)
 end
 
+function UIManager:FinishModifyMenu(p_Instance)
+	local s_UISquadCompData = UISquadCompData(p_Instance)
+
+	local s_ScreenAsset = UIScreenAsset(ResourceManager:SearchForDataContainer('UI/Flow/Screen/IngameMenuMP'))
+	s_ScreenAsset:MakeWritable()
+
+	local s_CenterHeaderNode = WidgetNode(s_ScreenAsset.nodes[m_WidgetIndexes.gamemodeInfoHeader])
+	local s_CenterHeaderBinding = UIPageHeaderBinding(s_CenterHeaderNode.dataBinding)
+	s_CenterHeaderBinding:MakeWritable()
+	s_CenterHeaderBinding.subHeader.dataCategory = s_UISquadCompData
+end
+
 function UIManager:_GetStaticItem(p_Name)
-    local s_ListItem = StaticListItem()
+	local s_ListItem = StaticListItem()
 	s_ListItem.itemName = p_Name
 
 	return s_ListItem
 end
 
 if g_UIManager == nil then
-    g_UIManager = UIManager()
+	g_UIManager = UIManager()
 end
 
 return g_UIManager
