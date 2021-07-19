3a113a5bc9bca3168758758b4eb25084b407c21f
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-07-20 01:40:56 +0200

    refactor(LevelNameHelper)

--- ext/Shared/Utils/LevelNameHelper.lua
+++ ext/Shared/Utils/LevelNameHelper.lua
@@ -2,16 +2,11 @@ class "LevelNameHelper"
 
 -- Returns "mp_001" from "levels/mp_001/mp_001"
 function LevelNameHelper:GetLevelName()
-	local s_LevelName = nil
-	local s_TempLevelName = SharedUtils:GetLevelName()
+	local s_LevelName = SharedUtils:GetLevelName()
 
-	if s_TempLevelName == nil then
+	if s_LevelName == nil then
 		return nil
 	end
 
-	for l_Word in string.gmatch(s_TempLevelName, "([^/]+)") do
-		s_LevelName = l_Word
-	end
-
-	return s_LevelName
+	return s_LevelName:gsub(".*/", "")
 end
