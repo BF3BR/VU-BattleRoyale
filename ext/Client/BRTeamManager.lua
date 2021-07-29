6f5f6fc2f451704d7c017d707af8666cd757fd05
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-07-30 01:24:22 +0200

    fix #193 (ClientTeamIds): fix for build 17669
    - use player.teamId instead of a method
    - also changing the teamIds of the soldiers

--- ext/Client/BRTeamManager.lua
+++ ext/Client/BRTeamManager.lua
@@ -35,12 +35,22 @@ end
 function BRTeamManager:OverrideTeamIds(p_Player, p_TeamId)
 	if p_Player == PlayerManager:GetLocalPlayer() or self:IsTeamMate(p_Player) then
 		m_Logger:Write("OverrideTeamId of player " .. p_Player.name .. " from " .. p_TeamId .. " to Team1")
-		p_Player:OverrideTeamId(TeamId.Team1)
-		p_Player:OverrideSquadId(SquadId.Squad1)
+		p_Player.teamId = TeamId.Team1
+		p_Player.squadId = SquadId.Squad1
+
+		if p_Player.soldier ~= nil then
+			m_Logger:Write("OverrideTeamId of soldier for this player from " .. p_TeamId .. " to Team1")
+			p_Player.soldier.teamId = TeamId.Team1
+		end
 	else
 		m_Logger:Write("OverrideTeamId of player " .. p_Player.name .. " from " .. p_TeamId .. " to Team2")
-		p_Player:OverrideTeamId(TeamId.Team2)
-		p_Player:OverrideSquadId(SquadId.Squad1)
+		p_Player.teamId = TeamId.Team2
+		p_Player.squadId = SquadId.Squad1
+
+		if p_Player.soldier ~= nil then
+			m_Logger:Write("OverrideTeamId of soldier for this player from " .. p_TeamId .. " to Team2")
+			p_Player.soldier.teamId = TeamId.Team2
+		end
 	end
 end
 
