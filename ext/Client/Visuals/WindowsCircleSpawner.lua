de8ea4b4a2da1a228679fa84113aaa639c650538
Nikos Kapraras <nikos@kapraran.dev>
2021-07-31 04:42:08 +0200

    use tabs in WindowsCircleSpawner

--- ext/Client/Visuals/WindowsCircleSpawner.lua
+++ ext/Client/Visuals/WindowsCircleSpawner.lua
@@ -4,10 +4,10 @@ class "WindowsCircleSpawner"
 
 local m_Logger = Logger("WindowsCircleSpawner", true)
 local m_ScalingMatrix = LinearTransform(
-  Vec3(1.0, 0.0, 0.0),
-  Vec3(0.0, 100.0, 0.0),
-  Vec3(0.0, 0.0, 1.0),
-  Vec3(0.0, 0.0, 0.0)
+	Vec3(1.0, 0.0, 0.0),
+	Vec3(0.0, 100.0, 0.0),
+	Vec3(0.0, 0.0, 1.0),
+	Vec3(0.0, 0.0, 0.0)
 )
 local m_MagicScalingNumber = 1.66
 
@@ -15,79 +15,72 @@ local m_MagicScalingNumber = 1.66
 local m_WindowBP = DC(Guid("C2F9C48C-A4EB-11E1-ABB8-FED5C2003E58"), Guid("11DEF780-CA1C-D8A7-A389-E267D1146509"))
 
 function WindowsCircleSpawner:__init()
-  self.m_Entities = {}
-  self.m_EntityData = nil
+	self.m_Entities = {}
+	self.m_EntityData = nil
 end
 
 function WindowsCircleSpawner:GetEntityData()
-  if self.m_EntityData ~= nil then
-    return self.m_EntityData
-  end
+	if self.m_EntityData ~= nil then
+		return self.m_EntityData
+	end
 
-  local s_ObjectBlueprint = m_WindowBP:GetInstance()
-  if s_ObjectBlueprint ~= nil then
-    self.m_EntityData = StaticModelEntityData(s_ObjectBlueprint.object)
-  end
+	local s_ObjectBlueprint = m_WindowBP:GetInstance()
+	if s_ObjectBlueprint ~= nil then
+		self.m_EntityData = StaticModelEntityData(s_ObjectBlueprint.object)
+	end
 
-  return self.m_EntityData
+	return self.m_EntityData
 end
 
 function WindowsCircleSpawner:SpawnWindow(p_From, p_To, p_EdgeLength)
-  -- create entity transform
+	-- create entity transform
 	local s_Angle = math.atan(p_To.z - p_From.z, p_To.x - p_From.x)
 	local s_EntityTrans = MathUtils:GetTransformFromYPR(-s_Angle, 0, 0)
 
-  -- TODO i think we need to use fixed y per map
+	-- TODO i think we need to use fixed y per map
 	s_EntityTrans.trans = Vec3(p_To.x, 125, p_To.z)
 
-  -- scale entity transform
+	-- scale entity transform
 	local s_XScaling = p_EdgeLength / m_MagicScalingNumber
 	m_ScalingMatrix.left.x = s_XScaling
 	s_EntityTrans = m_ScalingMatrix * s_EntityTrans
 
-  -- create entity
-  local s_EntityData = self:GetEntityData()
-  if s_EntityData == nil then
-    return
-  end
+	-- create entity
+	local s_EntityData = self:GetEntityData()
+	if s_EntityData == nil then
+		return
+	end
 	local s_CreatedEntity = EntityManager:CreateEntity(s_EntityData, s_EntityTrans)
 
-  if s_CreatedEntity ~= nil then
+	if s_CreatedEntity ~= nil then
 		s_CreatedEntity:Init(Realm.Realm_Client, true)
 		table.insert(self.m_Entities, s_CreatedEntity)
-
-		-- local s_SpatialEntity = SpatialEntity(s_CreatedEntity)
-		-- local s_Aabb = s_SpatialEntity.aabb
-
-    -- local width = s_Aabb.max.x - s_Aabb.min.x
-		-- local height = s_Aabb.max.y - s_Aabb.min.y
-		-- local depth = s_Aabb.max.z - s_Aabb.min.z
 	end
 end
 
 function WindowsCircleSpawner:DestroyEntities()
-  for _, l_Entity in pairs(self.m_Entities) do
-    l_Entity:Destroy()
-  end
+	for _, l_Entity in pairs(self.m_Entities) do
+		l_Entity:Destroy()
+	end
 
-  self.m_Entities = {}
+	self.m_Entities = {}
 end
 
 function WindowsCircleSpawner:Destroy()
-  self:DestroyEntities()
-  self.m_EntityData = nil
+	self:DestroyEntities()
+	self.m_EntityData = nil
 end
 
 function WindowsCircleSpawner:OnExtensionUnloading()
-  self:Destroy()
+	self:Destroy()
 end
 
 function WindowsCircleSpawner:OnLevelDestroy()
-  self:Destroy()
+	self:Destroy()
 end
 
 function WindowsCircleSpawner:__gc()
-  self:Destroy()
+	self:Destroy()
 end
 
 -- define global
