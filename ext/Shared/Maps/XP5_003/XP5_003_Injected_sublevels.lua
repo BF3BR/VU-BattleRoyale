9a19fb804c68ca980886a4654830df0bba77e53d
keku645 <36018654+keku645@users.noreply.github.com>
2021-08-17 19:21:35 +0200

    Removed prints from Injected sublevels

--- ext/Shared/Maps/XP5_003/XP5_003_Injected_sublevels.lua
+++ ext/Shared/Maps/XP5_003/XP5_003_Injected_sublevels.lua
@@ -15,7 +15,7 @@ if SharedUtils:GetLevelName() ~= 'Levels/XP5_003/XP5_003' then
     -- Airturbines + some props	---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     --print('Injecting PropsLarge reference data...')
-	print('PropsLarge spawned')
+	--print('PropsLarge spawned')
     local spLevelPropsLargeReferenceData = WorldPartReferenceObjectData(ResourceManager:FindInstanceByGuid(Guid('69AFE35D-259F-11E1-98E7-C42BEF8FFB67'), Guid('080E0293-4B9D-479C-A663-912BDC6CB24D'))) -- To change
     mpLevelPropsLargeReferenceData = WorldPartReferenceObjectData(spLevelPropsLargeReferenceData:Clone(Guid('A0000000-0000-0000-0000-000000000000')))
     mpLevelPropsLargeReferenceData:MakeWritable()
@@ -36,7 +36,7 @@ Events:Subscribe('Level:RegisterEntityResources', function(levelData)
     end
 
 
-    print('Adding new registry containing relevant SubWorldReferenceData...')
+    --print('Adding new registry containing relevant SubWorldReferenceData...')
     local newRegistry = RegistryContainer()
     newRegistry.referenceObjectRegistry:add(mpLevelPropsLargeReferenceData) -- PropsLarge
 
@@ -55,7 +55,7 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('7399C5F9-F745-522F-1A75-8C235D
     thisInstance:MakeWritable()
     local tent_01_Mesh = (50)
 	thisInstance.cullScale = tent_01_Mesh
-    print('Culldistance for tent_01_Mesh set.')
+    --print('Culldistance for tent_01_Mesh set.')
 end)
 
 --Bulkhead_01_Mesh
@@ -65,7 +65,7 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('BC838068-4D37-43D6-FCE2-D632D0
     thisInstance:MakeWritable()
     local Bulkhead_01_Mesh = (50)
 	thisInstance.cullScale = Bulkhead_01_Mesh
-    print('Culldistance for Bulkhead_01_Mesh set.')
+    --print('Culldistance for Bulkhead_01_Mesh set.')
 end)
 
 --PowerLine_01B_BrokenBase_Mesh
@@ -78,5 +78,5 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('1E857D81-FFDF-1EBC-D6F2-082F30
 	local PowerLine_01B_BrokenBase_Mesh_lod = (3)
 	thisInstance.lodScale = PowerLine_01B_BrokenBase_Mesh_lod
 	
-    print('Culldistance for PowerLine_01B_BrokenBase_Mesh set.')
+    --print('Culldistance for PowerLine_01B_BrokenBase_Mesh set.')
 end)
