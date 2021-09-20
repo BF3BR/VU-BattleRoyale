05ceeacf7f45895245e00905f05266704d65dbaf
keku645 <36018654+keku645@users.noreply.github.com>
2021-09-21 00:44:27 +0200

    Disabled destruction on X assets

--- ext/Shared/Maps/XP5_003/XP5_003_Injected_sublevels.lua
+++ ext/Shared/Maps/XP5_003/XP5_003_Injected_sublevels.lua
@@ -48,3 +48,160 @@ ResourceManager:RegisterInstanceLoadHandler(Guid('5F470049-4215-4E97-B07E-BA422A
 	p_Instance.cullDistance = 10000000.0
 	p_Instance.maxInstanceCount = 100
 end)
+
+
+-- =============================================
+-- Disable destruction on X assets
+-- =============================================
+
+--MEC_House_Low_02_V2
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('5F96ED33-D47A-ACB6-DE80-A59BFFD6485A'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('3FB5D38D-F259-A055-0559-17A37D6404AD'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('AFB1BACE-DE83-19DC-839B-00966E85C455'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('0CAE178D-66F5-1591-8D4C-8CF8EEC62207'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('892931DC-624D-DE2C-D2EE-C086E111A8DD'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('3FD0AFEA-F2FE-D6B6-F9D7-DC744E805F7B'), function(instance)
+   -- print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('A3081468-864B-74FC-EE5F-EC885A96F19F'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('AAA0BBAE-6D38-1D9D-B2EB-69FEE42B9183'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('A24D997F-673B-FC22-991E-A72578A69983'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('B8466EDB-F5C4-D425-42FC-060C6D7B01CE'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('BA441450-F0C0-19C6-69F9-F3777D0A9CBB'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('80366CDD-3E18-1120-6D66-7024EE30C514'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('855DE702-7FA6-11E0-8E76-FC4E665E8C40'), Guid('B196FFE0-EC20-7BBC-D403-B1FD1EF977CB'), function(instance)
+    --print('MEC_House_Low_02_V2 destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+
+--ME_House01_Garage_Destruction
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('C10FC461-FB35-D98F-DF5F-991EAC90D210'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('6763D0BF-C8F0-B2CB-5C47-5D3B71E84C31'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('334D0FB2-BC98-9F9F-193B-F72E64CE55DA'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('4EA2A78E-798B-2CCE-7A54-E735D0E3D714'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('851B14D5-5878-7A25-4059-6FA61601B87A'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('D722D65E-BA26-B4CA-1400-9E2CFFEAFCA2'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('CA3429E5-B1E5-8DE2-2D4F-8CA6035FC5B3'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
+
+ResourceManager:RegisterInstanceLoadHandler(Guid('4C8CED09-9BDE-11E0-A396-819D693420B6'), Guid('67409BE4-A1FC-F745-9F54-4052F655088D'), function(instance)
+    --print('ME_House01_Garage_Destruction destruction disabled...')
+    local thisInstance = BreakablePartComponentData(instance)
+    thisInstance:MakeWritable()
+    thisInstance.healthPercentage = 10000000
+end)
