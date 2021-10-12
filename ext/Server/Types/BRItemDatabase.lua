9ce3eb24a040e42b564aeb72e9072732429012d7
Nikos Kapraras <nikos@kapraran.dev>
2021-10-13 01:27:34 +0200

    fix(inventory): add support for split drops

--- ext/Server/Types/BRItemDatabase.lua
+++ ext/Server/Types/BRItemDatabase.lua
@@ -3,61 +3,72 @@ class "BRItemDatabase"
 local m_Logger = Logger("BRItemDatabase", true)
 
 function BRItemDatabase:__init()
-    -- A table of items (id -> BRItem)
-    self.m_Items = {}
+	-- A table of items (id -> BRItem)
+	self.m_Items = {}
 end
 
 -- Returns an item from the database, by it's id
 function BRItemDatabase:GetItem(p_Id)
-    return self.m_Items[p_Id]
+	return self.m_Items[p_Id]
 end
 
 -- All items should created using this methods from the database
 -- to keep it's integrity
 function BRItemDatabase:CreateItem(p_Definition, p_Quantity, p_Props)
-    p_Quantity = p_Quantity or 1
-    p_Props = p_Props or {}
+	p_Quantity = p_Quantity or 1
+	p_Props = p_Props or {}
 
-    -- create data table
-    local s_Table = {
-        Id = self:GetRandomId(),
-        UId = p_Definition.m_UId,
-        Quantity = math.max(1, math.min(p_Definition.m_MaxStack or 1, p_Quantity))
-    }
+	-- create data table
+	local s_Table = {
+		Id = self:GetRandomId(),
+		UId = p_Definition.m_UId,
+		Quantity = math.max(1, math.min(p_Definition.m_MaxStack or 1, p_Quantity))
+	}
 
-    -- extend and overwrite p_Props with s_Table data
-    for l_Key, l_Value in pairs(s_Table) do p_Props[l_Key] = l_Value end
+	-- extend and overwrite p_Props with s_Table data
+	for l_Key, l_Value in pairs(s_Table) do p_Props[l_Key] = l_Value end
 
-    -- create item instance and insert it to the items table
-    local s_Item = BRItem:CreateFromTable(p_Props)
-    self.m_Items[s_Item.m_Id] = s_Item
+	-- create item instance and insert it to the items table
+	local s_Item = BRItem:CreateFromTable(p_Props)
+	self.m_Items[s_Item.m_Id] = s_Item
 
-    -- m_Logger:Write("Item added to database. (" .. s_Item.m_Definition.m_Name .. ")")
+	-- m_Logger:Write("Item added to database. (" .. s_Item.m_Definition.m_Name .. ")")
 
-    return s_Item
+	return s_Item
 end
 
 -- Removes and destroys an item from the item database
 function BRItemDatabase:UnregisterItem(p_Id)
-    local s_Item = self:GetItem(p_Id)
-    if s_Item == nil then
-        return
-    end
+	local s_Item = self:GetItem(p_Id)
+	if s_Item == nil then
+		return
+	end
 
-    -- clear reference and destroy
-    self.m_Items[p_Id] = nil
-    s_Item:Destroy()
+	-- clear reference and destroy
+	self.m_Items[p_Id] = nil
+	s_Item:Destroy()
 
-    m_Logger:Write("Item removed from database. (" .. tostring(p_Id) .. ")")
+	m_Logger:Write("Item removed from database. (" .. tostring(p_Id) .. ")")
+end
+
+-- Creates a new item by removing an
+function BRItemDatabase:SplitItem(p_Id, p_QuantityToRemove)
+	local s_Item = self:GetItem(p_Id)
+	if s_Item == nil or not s_Item.m_Definition.m_Stackable or p_QuantityToRemove >= s_Item.m_Quantity then
+		return nil
+	end
+
+	s_Item:DecreaseQuantityBy(p_QuantityToRemove)
+	return self:CreateItem(s_Item.m_Definition, p_QuantityToRemove)
 end
 
 function BRItemDatabase:GetRandomId()
-    -- for now use the guid string
-    return tostring(MathUtils:RandomGuid())
+	-- for now use the guid string
+	return tostring(MathUtils:RandomGuid())
 end
 
 if g_BRItemDatabase == nil then
-    g_BRItemDatabase = BRItemDatabase()
+	g_BRItemDatabase = BRItemDatabase()
 end
 
 return g_BRItemDatabase
--- ext/Server/Types/BRLootPickupDatabase.lua
+++ ext/Server/Types/BRLootPickupDatabase.lua
@@ -11,93 +11,93 @@ local m_MapHelper = require "__shared/Utils/MapHelper"
 ---@param p_Transform Vec3|LinearTransform
 ---@param p_Items BRItem[]
 function BRLootPickupDatabase:CreateBasicLootPickup(p_Transform, p_Items)
-    self:CreateLootPickup(LootPickupType.Basic.Name, p_Transform, p_Items)
+	self:CreateLootPickup(LootPickupType.Basic.Name, p_Transform, p_Items)
 end
 
 function BRLootPickupDatabase:CreateAirdropLootPickup(p_Transform, p_Items)
-    self:CreateLootPickup(LootPickupType.Airdrop.Name, p_Transform, p_Items)
+	self:CreateLootPickup(LootPickupType.Airdrop.Name, p_Transform, p_Items)
 end
 
 function BRLootPickupDatabase:CreateLootPickup(p_Type, p_Transform, p_Items)
-    if p_Type == nil or p_Transform == nil or p_Items == nil then
-        m_Logger:Error("Invalid CreateLootPickup parameters")
-        return
-    end
+	if p_Type == nil or p_Transform == nil or p_Items == nil then
+		m_Logger:Error("Invalid CreateLootPickup parameters")
+		return
+	end
 
-    -- if p_Transform is Vec3, use it as the .trans of a LinearTransform
-    if type(p_Transform.z) == "number" then
-        local s_Trans = p_Transform
-        p_Transform = LinearTransform()
-        p_Transform.trans = s_Trans
-    end
+	-- if p_Transform is Vec3, use it as the .trans of a LinearTransform
+	if type(p_Transform.z) == "number" then
+		local s_Trans = p_Transform
+		p_Transform = LinearTransform()
+		p_Transform.trans = s_Trans
+	end
 
-    -- convert items array to map
-    local s_Items = {}
-    for _, l_Item in pairs(p_Items) do
-        s_Items[l_Item.m_Id] = l_Item
-    end
+	-- convert items array to map
+	local s_Items = {}
+	for _, l_Item in pairs(p_Items) do
+		s_Items[l_Item.m_Id] = l_Item
+	end
 
-    -- create item instance and insert it to the items table
-    local s_LootPickupId = self:GetRandomId()
-    local s_LootPickup = BRLootPickup(s_LootPickupId, p_Type, p_Transform, s_Items)
-    self.m_LootPickups[s_LootPickupId] = s_LootPickup
+	-- create item instance and insert it to the items table
+	local s_LootPickupId = self:GetRandomId()
+	local s_LootPickup = BRLootPickup(s_LootPickupId, p_Type, p_Transform, s_Items)
+	self.m_LootPickups[s_LootPickupId] = s_LootPickup
 
-    -- Spawn the entity server side as well if it has collision
-    if LootPickupType[p_Type].PhysicsEntityData ~= nil then
-        self.m_LootPickups[s_LootPickupId]:Spawn()
-    end
+	-- Spawn the entity server side as well if it has collision
+	if LootPickupType[p_Type].PhysicsEntityData ~= nil then
+		self.m_LootPickups[s_LootPickupId]:Spawn()
+	end
 
-    -- m_Logger:Write(string.format("LootPickup #%s was added", s_LootPickupId))
+	-- m_Logger:Write(string.format("LootPickup #%s was added", s_LootPickupId))
 
-    NetEvents:BroadcastLocal(InventoryNetEvent.CreateLootPickup, s_LootPickup:AsTable())
+	NetEvents:BroadcastLocal(InventoryNetEvent.CreateLootPickup, s_LootPickup:AsTable())
 end
 
 function BRLootPickupDatabase:Remove(p_LootPickup)
-    if not BRLootPickupDatabaseShared.Remove(self, p_LootPickup) then
-        return
-    end
+	if not BRLootPickupDatabaseShared.Remove(self, p_LootPickup) then
+		return
+	end
 
-    -- remove remaining items from item database
-    for _, l_Item in pairs(p_LootPickup.m_Items) do
-        m_ItemDatabase:UnregisterItem(l_Item.m_Id)
-    end
+	-- remove remaining items from item database
+	for _, l_Item in pairs(p_LootPickup.m_Items) do
+		m_ItemDatabase:UnregisterItem(l_Item.m_Id)
+	end
 
-    p_LootPickup:Destroy()
+	p_LootPickup:Destroy()
 
-    NetEvents:BroadcastLocal(InventoryNetEvent.UnregisterLootPickup, p_LootPickup.m_Id)
+	NetEvents:BroadcastLocal(InventoryNetEvent.UnregisterLootPickup, p_LootPickup.m_Id)
 
-    m_Logger:Write("Loot Pickup removed from database.")
+	m_Logger:Write("Loot Pickup removed from database.")
 end
 
 function BRLootPickupDatabase:RemoveItemFromLootPickup(p_LootPickupId, p_ItemId)
-    local s_LootPickup = self.m_LootPickups[p_LootPickupId]
-    if s_LootPickup == nil then
-        return
-    end
+	local s_LootPickup = self.m_LootPickups[p_LootPickupId]
+	if s_LootPickup == nil then
+		return
+	end
 
-    s_LootPickup:RemoveItem(p_ItemId)
+	s_LootPickup:RemoveItem(p_ItemId)
 
-    if not m_MapHelper:Empty(s_LootPickup.m_Items) then
-        -- Broadcast updated lootPickup
-        NetEvents:BroadcastLocal(InventoryNetEvent.UpdateLootPickup, s_LootPickup:AsTable())
-    else
-        -- Remove loot pickup if all the item got picked up
-        self:Remove(s_LootPickup)
-    end
+	if not m_MapHelper:Empty(s_LootPickup.m_Items) then
+		-- Broadcast updated lootPickup
+		NetEvents:BroadcastLocal(InventoryNetEvent.UpdateLootPickup, s_LootPickup:AsTable())
+	else
+		-- Remove loot pickup if all the item got picked up
+		self:Remove(s_LootPickup)
+	end
 end
 
 function BRLootPickupDatabase:UpdateState(p_LootPickupId)
-    local s_LootPickup = self.m_LootPickups[p_LootPickupId]
-    if s_LootPickup == nil then
-        return
-    end
+	local s_LootPickup = self.m_LootPickups[p_LootPickupId]
+	if s_LootPickup == nil then
+		return
+	end
 
-    NetEvents:BroadcastLocal(InventoryNetEvent.UpdateLootPickup, s_LootPickup:AsTable())
+	NetEvents:BroadcastLocal(InventoryNetEvent.UpdateLootPickup, s_LootPickup:AsTable())
 end
 
 function BRLootPickupDatabase:GetRandomId()
-    -- for now use the guid string
-    return tostring(MathUtils:RandomGuid())
+	-- for now use the guid string
+	return tostring(MathUtils:RandomGuid())
 end
 
 -- TODO: OnPlayerJoin send the whole self.m_LootPickups to that player
