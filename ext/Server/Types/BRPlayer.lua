8056c0ce86cb9a8ac0f53f004e7545cc4574f6c4
kaloczikvn <kaloczi.kvn@gmail.com>
2021-12-07 20:56:47 +0100

    tweak(BRPlayer): remove unused CreateCustomizeSoldierData function

--- ext/Server/Types/BRPlayer.lua
+++ ext/Server/Types/BRPlayer.lua
@@ -270,35 +270,6 @@ function BRPlayer:Spawn(p_Trans)
 	end)
 end
 
--- TODO move to a util
-function BRPlayer:CreateCustomizeSoldierData()
-	local s_CustomizeSoldierData = CustomizeSoldierData()
-	s_CustomizeSoldierData.restoreToOriginalVisualState = false
-	s_CustomizeSoldierData.clearVisualState = true
-	s_CustomizeSoldierData.overrideMaxHealth = -1.0
-	s_CustomizeSoldierData.overrideCriticalHealthThreshold = -1.0
-
-	local s_UnlockWeaponAndSlot7 = UnlockWeaponAndSlot()
-	s_UnlockWeaponAndSlot7.weapon = SoldierWeaponUnlockAsset(
-		ResourceManager:FindInstanceByGuid(Guid("0003DE1B-F3BA-11DF-9818-9F37AB836AC2"),Guid("8963F500-E71D-41FC-4B24-AE17D18D8C73"))
-	)
-	s_UnlockWeaponAndSlot7.slot = WeaponSlot.WeaponSlot_7
-	s_CustomizeSoldierData.weapons:add(s_UnlockWeaponAndSlot7)
-
-	local s_UnlockWeaponAndSlot9 = UnlockWeaponAndSlot()
-	s_UnlockWeaponAndSlot9.weapon = SoldierWeaponUnlockAsset(
-		ResourceManager:FindInstanceByGuid(Guid("7C58AA2F-DCF2-4206-8880-E32497C15218"),Guid("B145A444-BC4D-48BF-806A-0CEFA0EC231B"))
-	)
-	s_UnlockWeaponAndSlot9.slot = WeaponSlot.WeaponSlot_9
-	s_CustomizeSoldierData.weapons:add(s_UnlockWeaponAndSlot9)
-
-	s_CustomizeSoldierData.activeSlot = WeaponSlot.WeaponSlot_7
-	s_CustomizeSoldierData.removeAllExistingWeapons = true
-	s_CustomizeSoldierData.disableDeathPickup = false
-
-	return s_CustomizeSoldierData
-end
-
 -- =============================================
 	-- Spectator Functions
 -- =============================================
